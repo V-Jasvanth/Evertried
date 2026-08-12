ac919665b1e543eed294b6f04604241803df8656
V-Jasvanth <jasvanthjasvanth21@gmail.com>
2026-08-12 16:55:51 +0530

    Update backend database configuration and server

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
+        console.log("========== FULL ERROR ==========");
+        console.dir(error, { depth: null });
+        console.log("================================");
         process.exit(1);
     }
 };
