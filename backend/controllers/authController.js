a53ea8007ac4dea24b8979cd628893f8ba1dad75
Jashu <155837757+V-Jasvanth@users.noreply.github.com>
2026-08-20 22:18:26 +0530

    Fix indentation in authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,6 @@ const generateToken = (id) => {
         { expiresIn: '30d' }
     );
 };
-
 // --------------------------------------------------
 // GMAIL SMTP CONFIGURATION
 // --------------------------------------------------
@@ -22,6 +21,7 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+        
     }
 });
 
